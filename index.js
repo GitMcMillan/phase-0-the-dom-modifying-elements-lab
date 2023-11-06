906578369e293d3311a2d5788bbd85fa8ff19ea0
alexandermcmillan <mcmillancontact@protonmail.com>
2023-11-05 22:07:19 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 // Write your code here!
 
-document.getElementById("main").remove();
+const mainRemove = document.querySelector("main")
+mainRemove.remove()
 
-let newHeader = document.createElement(`h1`)
+
+let newHeader = document.createElement("h1")
+// document.body.append(newHeader)
+
 newHeader.setAttribute(`id`, `victory`);
-newHeader.innerHTML = `Alex is the champion`;
+newHeader.textContent = `Alex is the champion`;
+
+
+// let newHeader = document.createElement(`h1`)
+// newHeader.setAttribute(`id`, `victory`);
+// newHeader.innerHTML = `Alex is the champion`;
+
+// function update() {
+//   let newHeader = document.querySelector("h1")
+//   changed.id = `victory`
+// }
+
+// update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("main").remove();
+
+// let newHeader = document.createElement(`h1`)
+// newHeader.setAttribute(`id`, `victory`);
+// newHeader.innerHTML = `Alex is the champion`;
